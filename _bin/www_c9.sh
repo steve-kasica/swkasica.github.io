6655d48ca5525adcc15e3310158077e1ef1244c7
Stephen Wayne Kasica <hello@stephenkasica.com>
2018-11-05 02:50:21 +0000

    Update for c9 build environment

---
+++ _bin/www_c9.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+jekyll serve --host $IP --port $PORT
--- _site/js/site.js
+++ _site/js/site.js
@@ -24,95 +24,95 @@ $(document).ready(function() {
     });
     
     // Initialize isotope filtering
-    // $('.isotope').each(function(i,el) {
-    //     var $el = $(el);
-    //     var $header = $(el).siblings('.header');
+    $('.isotope').each(function(i,el) {
+        var $el = $(el);
+        var $header = $(el).siblings('.header');
         
-    //     $el.filterSettings = (function() {
-    //         var out = {};
-    //         $header.find('.filter a').each(function(i, el) {
-    //             var $el = $(el);
-    //             if ($el.attr('datakey') === 'checkbox') {
-    //                  out[$el.attr('datavalue')] = ($el.hasClass('active')) ? true : false;
-    //             } else if (!$el.hasClass(hidden)) {
-    //                 out[$el.attr('datakey')] = $el.attr('datavalue');
-    //             }
-    //         });
-    //         return out;
-    //     })();
+        $el.filterSettings = (function() {
+            var out = {};
+            $header.find('.filter a').each(function(i, el) {
+                var $el = $(el);
+                if ($el.attr('datakey') === 'checkbox') {
+                     out[$el.attr('datavalue')] = ($el.hasClass('active')) ? true : false;
+                } else if (!$el.hasClass(hidden)) {
+                    out[$el.attr('datakey')] = $el.attr('datavalue');
+                }
+            });
+            return out;
+        })();
         
-    //     $el.isotope({
-    //         itemSelector: '.item-container',
-    //         percentPosition: true,
-    //         layoutMode: $el.attr('data-layout'),
-    //         masonry: {
-    //             // use outer width of grid-sizer for columnWidth
-    //             columnWidth: (function(mode) {
-    //                 return (mode === 'masonry') ? '.grid-sizer' : '100%';
-    //             })($el.attr('data-layout')),
-    //         },
-    //         stamp: '.stamp',
-    //         getSortData: {
-    //             'post_title': '[data-title]',
-    //             'post_coolness': '[data-coolness] parseInt',
-    //         },
-    //         animationOptions: {
-    //             duration: 200,
-    //             queue: false,
-    //             easing: 'linear',
-    //         },
-    //     });
+        $el.isotope({
+            itemSelector: '.item-container',
+            percentPosition: true,
+            layoutMode: $el.attr('data-layout'),
+            masonry: {
+                // use outer width of grid-sizer for columnWidth
+                columnWidth: (function(mode) {
+                    return (mode === 'masonry') ? '.grid-sizer' : '100%';
+                })($el.attr('data-layout')),
+            },
+            stamp: '.stamp',
+            getSortData: {
+                'post_title': '[data-title]',
+                'post_coolness': '[data-coolness] parseInt',
+            },
+            animationOptions: {
+                duration: 200,
+                queue: false,
+                easing: 'linear',
+            },
+        });
     
-    //     $el.css('min-height', Number($el.css('height').replace('px','')) + 10);
+        $el.css('min-height', Number($el.css('height').replace('px','')) + 10);
 
-    //     // Trigger isotope filtering 
-    //     $header.find('.filter a').click(function(e) {
-    //         e.preventDefault();
-    //         var $a = $(this);
-    //         var key = $a.attr('datakey');
-    //         var val = $a.attr('datavalue');
+        // Trigger isotope filtering 
+        $header.find('.filter a').click(function(e) {
+            e.preventDefault();
+            var $a = $(this);
+            var key = $a.attr('datakey');
+            var val = $a.attr('datavalue');
             
-    //         if (key === 'checkbox') {
-    //             $el.filterSettings[val] = !$el.filterSettings[val];
-    //         } else {
-    //             $el.filterSettings[key] = $header.find('a[datakey=' + key + ']').not('.' + hidden).attr('datavalue');
-    //         }
+            if (key === 'checkbox') {
+                $el.filterSettings[val] = !$el.filterSettings[val];
+            } else {
+                $el.filterSettings[key] = $header.find('a[datakey=' + key + ']').not('.' + hidden).attr('datavalue');
+            }
             
-    //         $el.isotope({
-    //             filter: (function() {
-    //                 var facets = [];
-    //                 var isXor = false;
-    //                 var delim, out;
+            $el.isotope({
+                filter: (function() {
+                    var facets = [];
+                    var isXor = false;
+                    var delim, out;
                     
-    //                 for (var prop in $el.filterSettings) {
-    //                     if (typeof $el.filterSettings[prop] === 'boolean' && $el.filterSettings[prop]) {
-    //                         facets.push('.' + prop);
-    //                     }
-    //                 }
+                    for (var prop in $el.filterSettings) {
+                        if (typeof $el.filterSettings[prop] === 'boolean' && $el.filterSettings[prop]) {
+                            facets.push('.' + prop);
+                        }
+                    }
                     
-    //                 switch($el.filterSettings.logic) {
-    //                     case 'and': delim = ''; break;
-    //                     case 'xor': isXor = true;
-    //                     case 'or': delim = ', '; break;
-    //                 }
+                    switch($el.filterSettings.logic) {
+                        case 'and': delim = ''; break;
+                        case 'xor': isXor = true;
+                        case 'or': delim = ', '; break;
+                    }
                     
-    //                 if (isXor) {
-    //                     var not = ':not(' + facets.join('') + ')';
-    //                     out = facets.map(function(el) {
-    //                         if (facets.length > 1)
-    //                             return el + not;
-    //                         else
-    //                             return el;
-    //                     }).join(delim);
-    //                 } else {
-    //                     out = facets.join(delim);
-    //                 }
-    //                 return out;
-    //             })(),
-    //             sortBy: 'post_' + $el.filterSettings.sort,
-    //             sortAscending: ($el.filterSettings.order === 'asc'),
-    //         });
-    //     }); // click filter > a click
-    // }); // for each .isotope
+                    if (isXor) {
+                        var not = ':not(' + facets.join('') + ')';
+                        out = facets.map(function(el) {
+                            if (facets.length > 1)
+                                return el + not;
+                            else
+                                return el;
+                        }).join(delim);
+                    } else {
+                        out = facets.join(delim);
+                    }
+                    return out;
+                })(),
+                sortBy: 'post_' + $el.filterSettings.sort,
+                sortAscending: ($el.filterSettings.order === 'asc'),
+            });
+        }); // click filter > a click
+    }); // for each .isotope
 
 }); // $(document).ready
